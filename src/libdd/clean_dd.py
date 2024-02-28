cc4131d4449c5b84fafa416512bcd779fd1deb54
Ann Holmes <ann.142.holmes@gmail.com>
2024-02-28 14:45:47 -0500

    Moved comment into docstring

--- src/libdd/clean_dd.py
+++ src/libdd/clean_dd.py
@@ -1,15 +1,17 @@
-''' Clean Null Terms from Data Dictionaries
-Data dictionary as csv [read_csv(ddfile)] -> Data dictionary to JSON [ddtoJSON(ddfile)] -> 
-Data Dictionary clean null terms [cleanNullTerms(ddJSON)]
 
-ddJSON input argument - Data dictionary in JSON/Dictionary format so function dd_toJSON() should be run 
-first on a csv data dictionary file one wants to transform
-
-k,v - iterations to go through nested data dictionary structure in JSON/Dictionary format and remove
-instances where the value of a key is equal to 0 (see _missingRepresentation in dd_toJSON.py)
-'''
 
 def cleanNullTerms(ddJSON):
+   ''' Clean Null Terms from Data Dictionaries
+    Data dictionary as csv [read_csv(ddfile)] -> Data dictionary to JSON [ddtoJSON(ddfile)] -> 
+    Data Dictionary clean null terms [cleanNullTerms(ddJSON)]
+
+    ddJSON input argument - Data dictionary in JSON/Dictionary format so function dd_toJSON() should be run 
+    first on a csv data dictionary file one wants to transform
+
+    k,v - iterations to go through nested data dictionary structure in JSON/Dictionary format and remove
+    instances where the value of a key is equal to 0 (see _missingRepresentation in dd_toJSON.py)
+    '''
+
    clean = {}
    for k, v in ddJSON.items():
         if isinstance(v, dict):
